d542f758a4d4ab7cafe86c7d3e830fd0b8bcbefb
Alex Moreno <alex.moreno.costa@gmail.com>
2021-07-01 23:10:30 -0300

    aula-2

--- main.go
+++ main.go
@@ -9,7 +9,14 @@ type ContaCorrente struct {
 	saldo   float64
 }
 
+func (cc *ContaCorrente) Sacar(valor float64) {
+	if valor > 0 && valor < cc.saldo {
+		cc.saldo -= valor
+	}
+}
+
 func main() {
 	contaAlex := ContaCorrente{"Alex Moreno", 123, 12345678, 2000}
+	contaAlex.Sacar(-1800)
 	fmt.Println(contaAlex)
 }
